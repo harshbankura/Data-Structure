294817978991bbfb662b737036feb39da512a3fe
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-04-11 09:00:32 +0530

    Time: 48 ms (73.70%) | Memory: 167.9 MB (39.13%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int row=isWater.length;
+        int col=isWater[0].length;
+        int[][] result = new int[row][col];
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(isWater[i][j]==1){
+                    q.offer(new int[]{i,j});
+                }else{
+                    result[i][j]=-1;
+                }
+            }
+        }
+        int[][]dir = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int x=curr[0];
+            int y=curr[1];
+            for(int[]d:dir){
+                int xn=x+d[0];
+                int yn=y+d[1];
+                if(xn>=0&&xn<row&&yn>=0&&yn<col&&result[xn][yn]==-1){
+                    result[xn][yn]=result[x][y]+1;
+                    q.offer(new int[]{xn,yn});
+                }
+            }
+        }
+        return result;
+    }
+}
