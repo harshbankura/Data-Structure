fb28f47bbf1b6a7b52f37c4c6f71d68e6b90f52c
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-04-15 15:39:49 +0530

    Time: 7 ms (51.00%) | Memory: 44.9 MB (89.44%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] pre:prerequisites){
+            adj.get(pre[0]).add(pre[1]);
+        }
+        int[] ind= new int[n];
+        for(int i=0;i<n;i++){
+            for(int neigh:adj.get(i)){
+                ind[neigh]++;
+            }
+        }
+        Queue<Integer> q= new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(ind[i]==0){
+                q.offer(i);
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            result.add(curr);
+            for(int neigh:adj.get(curr)){
+                ind[neigh]--;
+                if(ind[neigh]==0){
+                    q.offer(neigh);
+                }
+            }
+
+        }
+        return result.size()==n;
+
+
+        
+    }
+}
