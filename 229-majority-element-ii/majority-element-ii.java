9011b3e7ddc5d6879bc81e3eee33d1cc12d0c28d
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-06-02 21:10:55 +0530

    Time: 2 ms (99.46%) | Memory: 50.4 MB (19.88%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,43 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n =nums.length;
-        int c1=0;
-        int c2=0;
-        int el1=0;
-        int el2=0;
+        int n=nums.length;
+        int e1=0;
+        int e2=0,c1=0,c2=0;
         for(int num:nums){
-            if(num==el1){
+            if(e1==num){
                 c1++;
-            }
-            else if(num==el2){
+            }else if(e2==num){
                 c2++;
-            }
-            else if(c1==0){
-                el1=num;
+            }else if(c1==0){
+                e1=num;
                 c1++;
-            }
-            else if(c2==0){
-                el2=num;
+            }else if(c2==0){
+                e2=num;
                 c2++;
-            }
-            else{
+            }else{
                 c1--;
                 c2--;
             }
         }
+        // verify
+        List<Integer> ans = new ArrayList<>();
         c1=0;
         c2=0;
         for(int num:nums){
-            if(num==el1){
+            if(num==e1){
                 c1++;
-            }
-            else if(num==el2){
-                c2++;
-            }
+
+            }else if(num==e2) c2++;
+
         }
-        List<Integer> ans = new ArrayList<>();
-        if(c1>n/3) ans.add(el1);
-        if (c2>n/3) ans.add(el2);
+        if(c1>n/3) ans.add(e1);
+        if(c2>n/3) ans.add(e2);
         return ans;
         
     }
