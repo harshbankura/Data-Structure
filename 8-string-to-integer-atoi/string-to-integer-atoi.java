f1a7d96a8362cbddb65c26768f1ea5949862a8ed
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-06-14 11:20:23 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (23.20%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,29 +1,32 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s==null||s.length()==0)return 0;
-        int index=0;
-        int sign=1;
+        int i=0;
         int n=s.length();
-        int result=0;
-        while(index<n&&s.charAt(index)==' '){
-            index++;
+        long result=0;
+        int sign=1;
+        while(i<n&&s.charAt(i)==' '){
+            i++;
         }
-        if(index<n&& (s.charAt(index)=='-'||s.charAt(index)=='+')){
-            sign=s.charAt(index)=='-'?-1:1;
-            index++;
-        }
-        while(index<n && Character.isDigit(s.charAt(index))){
-            int ans= s.charAt(index)-'0';
-            // 32 bit condition min max;
-            if (result > (Integer.MAX_VALUE - ans) / 10) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        if(i<n&&(s.charAt(i)=='+'||s.charAt(i)=='-')){
+            if(s.charAt(i)=='-'){
+                sign=-1;
             }
-            // end
-            result=result*10+ans;
-            index++;
+            i++;
+
 
         }
-        return result*sign;
-        
+        while(i<n&&Character.isDigit(s.charAt(i))){
+            int num=s.charAt(i)-'0';
+            result=result*10 +num;
+            // Step 4: Clamp if out of int range
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+        return (int)result*sign;
     }
 }
