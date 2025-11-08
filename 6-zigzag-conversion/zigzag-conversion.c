536437eb5304f72e1e9dfe4f732d55f156827e2c
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-11-08 12:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (75.13%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.c
@@ -0,0 +1,47 @@
+char* convert(char* s, int numRows) {
+     int len = strlen(s);
+    char *ret = (char *)malloc(len + 1);
+    int i, j;
+    j = 0; /* index of return string */
+
+    /**
+    * don't know why return s or modify input pointer will cause runtime error, 
+    * so I have to copy a string and try not to modify s. I tried 5 times to
+    * finally find out what happened, sad for my acceptance rate...
+    */
+    if (numRows == 1) {
+        while (j < len) {
+            ret[j] = s[j];
+            j++;
+        }
+        ret[len] = '\0';
+        return ret;
+    }
+
+    int t = (numRows - 1) * 2;
+    int shift, offset;
+    
+    for (i = 0; i < numRows; i++) {
+        offset = 0;
+        shift = (numRows - i - 1) * 2;
+        while (j < len && i + offset < len) {
+            ret[j] = s[i + offset];
+            j++;
+
+            if (shift == 0) {
+                shift = t;
+            }
+
+            offset += shift;
+
+            if (shift != t) {
+                shift = t - shift;
+            }
+        }
+    }
+
+    ret[len] = '\0';
+
+    return ret;
+    
+}
